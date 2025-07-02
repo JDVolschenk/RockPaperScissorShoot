bb51c0ca7c8373bf4ece37f13d0a282f470b30a5
JD Volschenk <SHA256:1RYvXXm/3lhTWS0gbb+5F19YTUJtcCnlKEIaWVP3Tiw>
2025-07-02 13:40:08 +0200

    edited index.js

--- index.js
+++ index.js
@@ -2,27 +2,76 @@
 /*--Computer Logic--*/
 
 
-function theComputerChoice () {
+function theComputerChoice (computerInput) {
                    const computerChoice = Math.floor(Math.random() * 3);
                    if (computerChoice===1) {
                     console.log("ROCK");
+                      return "ROCK";
                    } else if (computerChoice===2) {
                     console.log("PAPER");
+                    return "PAPER";
                    }  if (computerChoice===0) {
                     console.log("SCISSOR");
-                   } else if (computerChoice===3) {
-                    console.log("SCISSOR");
-                   }
+                        return "SCISSOR";
+                   }     
+                   return computerChoice ; 
 }
 
-theComputerChoice();
 
 /*--Human Logic--*/
 
 function theUserChoice () {
-    const userChoice = prompt("ROCK, PAPER OR SCISSOR?");
+    const userChoice = prompt("ROCK, PAPER OR SCISSOR?"); 
+    return userChoice ;
 }
 
-theUserChoice();
-theUserChoice();
 
+/*--Score Variable--*/
+
+let userScore = 0
+let computerScore = 0 
+
+/*--Play ONE round--*/
+
+function oneRound (userInput, computerInput) {
+    
+
+    userInput = userInput.toUpperCase();
+
+    
+
+
+    if (userInput===computerInput) {
+        console.log("TIE");}
+       else if   (
+                    (userInput==="ROCK" && computerInput==="SCISSOR") || 
+                    (userInput==="PAPER" && computerInput==="ROCK") ||
+                    (userInput==="SCISSOR" && computerInput==="PAPER") 
+                 ) {
+                            userScore++;
+                        }  
+                 else {
+                    computerScore++;
+                      }
+
+                console.log(`userScore=${userScore}` +" "+ `computerScore=${computerScore}`);
+            
+    }
+        
+    
+     
+   
+const userInput = theUserChoice();
+const computerInput = theComputerChoice();
+
+oneRound(userInput, computerInput);
+oneRound(userInput,computerInput);
+
+
+
+
+
+
+
+
+``
